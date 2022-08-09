66887daa13a04235637b73be05ff154cb81de50c
CoBeTcKuu <9437855@gmail.com>
2022-08-09 19:11:57 +0300

    коммит задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //задача 1
         int j = 1 ;
                 while (j<11){
                     System.out.print(" " + j++);
@@ -7,6 +8,9 @@ public class Main {
         System.out.println("\n");
                 for (int i = 10; i>0; i--) {
             System.out.print(" " +i);
+        //задача 2
+
+
         }
     }
 }
