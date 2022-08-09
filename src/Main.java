8f943bac0e607d45ebea81b3159dcb4be1ef62cf
CoBeTcKuu <9437855@gmail.com>
2022-08-09 19:10:15 +0300

    коммит задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int j = 1 ;
+                while (j<11){
+                    System.out.print(" " + j++);
+                }
+        System.out.println("\n");
+                for (int i = 10; i>0; i--) {
+            System.out.print(" " +i);
+        }
     }
 }
