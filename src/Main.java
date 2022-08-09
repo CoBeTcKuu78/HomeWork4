f5b1f6eda723398bdde40bb905bfdf0955486541
CoBeTcKuu <9437855@gmail.com>
2022-08-09 23:03:44 +0300

    коммит задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //задача 1
-        int j = 1 ;
-                while (j<11){
-                    System.out.print(" " + j++);
-                }
-        System.out.println("\n");
-                for (int i = 10; i>0; i--) {
-            System.out.print(" " +i);
         //задача 2
-
-
-        }
+    int firstFriday = 5;
+    int nextFriday = firstFriday + 7;
+    int lastFriday = 31;
+    for (int i = firstFriday; i < lastFriday; i = i + 7){
+        System.out.println( "Сегодня пятница " + i  + "-е, подготовьте отчет!");
+        //break;
+    }
     }
 }
