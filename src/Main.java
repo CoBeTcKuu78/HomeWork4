2276c77d2facf6d81206a20850e9c906dff82169
CoBeTcKuu <9437855@gmail.com>
2022-08-10 19:44:56 +0300

    коммит задача 2 и задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+
         //задача 2
     int firstFriday = 5;
     int nextFriday = firstFriday + 7;
@@ -8,5 +10,24 @@ public class Main {
         System.out.println( "Сегодня пятница " + i  + "-е, подготовьте отчет!");
         //break;
     }
+        //задача 3
+        int yearNow = 2022;
+        int begin = yearNow -200;
+        int end = yearNow + 100;
+        for (int i = 0; i <= end; i = i + 79)
+            if(i >= begin){
+            System.out.println(i);
+        }
+
+
+        /* не моё решение,но смотрел и сверял
+        int currentYear = 2022;
+        int begin = currentYear - 200;
+        int end = currentYear + 100;
+        for (int year = 0; year <= end; year += 79) {
+            if (year >= begin) {
+                System.out.println(year);
+            }
+        }*/
     }
 }
